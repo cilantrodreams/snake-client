905368498c60a6cb8555d9a0ce934dc632a72b76
Thomas Ngo <thomasngo818@gmail.com>
2022-01-19 23:05:52 +0000

    Added play.js file to test connecting to snake game server

---
+++ play.js
@@ -0,0 +1,15 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+}
+
+console.log(`connecting...`);
+connect();
