f405e5d24ba46dd03c7d216c887bf0ea94ddeb63
Thomas Ngo <thomasngo818@gmail.com>
2022-01-19 23:13:48 +0000

    Added client.js and moved connect function from play into it. Modified play to use exports

--- play.js
+++ play.js
@@ -1,15 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log(`connecting...`);
 connect();
