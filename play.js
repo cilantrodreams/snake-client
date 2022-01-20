d10afec1f0f57e6e4297b30a98e9dcb40732f6a2
Thomas Ngo <thomasngo818@gmail.com>
2022-01-20 00:01:48 +0000

    Added function to accept keyboard inputs. added function to look for ctrl-c termination input

--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const { connect } = require('./client');
 
 console.log(`connecting...`);
 connect();
+
+// setup interface to handle user input from stdin
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput()
